231141f3edcc047520ef96803cebf21bed3c89ca
Thiago de Matos <oxumourinho@gmail.com>
2026-05-25 17:19:59 -0300

    exercicio01

---
+++ Dia 4/exercicios01.js
@@ -0,0 +1,11 @@
+let fome = prompt("Esta com fome? (sim/nao) ");
+let dinheiro = prompt("Voce tem dinheiro? (sim/nao) ");
+let restaurante = prompt("Restaurante esta aberto? (sim/nao) ");
+
+if(fome === "nao" || dinheiro === "nao"){
+    console.log("Hoje a janta sera em casa")
+}else if(dinheiro === "sim" && restaurante === "nao"){
+    console.log("Peça um delivery")
+}else{
+    console.log("hoje o jantar sera no seu restaurante favorito!")
+};
